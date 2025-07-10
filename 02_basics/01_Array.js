d657e95ae0955d629d70ba7a8cd9607ff0195fa5
ggsipu <suryansh030806@gmail.com>
2025-07-10 08:26:42 +0000

    arrays done

--- 02_basics/01_Array.js
+++ 02_basics/01_Array.js
@@ -11,7 +11,6 @@ console.log(myArr)
 console.log(myArr[2])
 
 //Array Methods
-
 console.log(arr.length) //length of array
 console.log(arr[arr.length - 1]) //last element of array
 myArr.push(7) //adds element at the end of array
@@ -23,3 +22,32 @@ myArr.unshift(8) //adds element at the beginning of array
 console.log(myArr)
 myArr.shift() //removes first element of array
 console.log(myArr)
+
+// console.log(myArr.indexOf(5)) //returns index of element
+// console.log(myArr.indexOf(9)) //returns -1
+// console.log(myArr.includes(5)) //returns true if element is present
+// console.log(myArr.reverse()) //Reverse the array
+// console.log(myArr.sort()) //Sorts the array in ascending order
+// console.log(myArr.slice(1, 3)) //returns a shallow copy of a portion
+
+const newArr = [0,1,2,3,4,5,6]
+const jArr = newArr.join()//joins all elements of an array into a string with given separator
+console.log(typeof jArr) //string type
+console.log(jArr) //0,1,2,3,4,5,6
+
+
+//Slice and Splice
+console.log("A",myArr)
+
+const myN1 = myArr.slice(1,3)//returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included)
+console.log(myN1) //returns a new array with elements from index
+console.log("B",myArr) //returns a new array with elements from index
+
+const myN2 = myArr.splice(1,3)//changes the contents of an array by removing or replacing existing elements and/or adding new elements in place
+console.log(myN2) //returns an array with removed elements
+console.log("C",myArr) //returns a new array with elements from index
+
+//Difference between slice and splice:-
+//In splice, array is manipulated but in slice, array is not manipulated
+//splice modifies the original array, slice does not modify the original array
+//splice can add or remove elements, slice only returns a portion of the array
---
+++ 02_basics/02_Arryas_2.js
@@ -0,0 +1,30 @@
+const marvel_heros = ["thor","ironman","spiderman"]
+const dc_heros = ["flash","superman","batman"]//we can also take any data type in array
+
+//marvel_heros.push(dc_heros)//recieves array dc_heros as a single element
+//console.log(marvel_heros)//[ 'thor', 'ironman', 'spiderman', [ 'flash', 'superman', 'batman' ] ]
+
+//const all_heroes = marvel_heros.concat(dc_heros)
+//console.log(all_heroes)//[ 'thor', 'ironman', 'spiderman', 'flash', 'superman', 'batman' ]
+
+
+const newHeros = [...marvel_heros,...dc_heros]//spread operator
+console.log(newHeros)//[ 'thor', 'ironman', 'spiderman', 'flash', 'superman', 'batman' ]
+
+const another_arr = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+let my_real_flat_arr_depth2 = another_arr.flat(2)//flattens the array to 2 levels deep
+let my_real_flat_arr_depth1 = another_arr.flat(1)//flattens the array to 1 level deep
+console.log(my_real_flat_arr_depth2)//[ 1, 2, 3, 4, 5, 6, 7, 6, 7, 4, 5 ]
+console.log(my_real_flat_arr_depth1)//[ 1, 2, 3, 4, 5, 6, 7, 6, 7, [ 4, 5 ] ]
+
+
+
+console.log(Array.isArray("Suryansh"))//false
+console.log(Array.from("Suryansh"))//true
+console.log(Array.from({name:"Suryansh"}))//returns empty array as it is defined that whom's array has to create : keys or values
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))//[ 100, 200, 300 ] //creates an array of the given values and returns it
