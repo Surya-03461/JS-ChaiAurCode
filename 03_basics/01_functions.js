039d97cdb5e3937d9077b54ce28a9268851e7bbe
ggsipu <suryansh030806@gmail.com>
2025-07-14 16:11:33 +0000

    functions done!!

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,59 @@
+//Function Definition
+function myName() {
+    console.log("S");
+    console.log("U");
+    console.log("R");
+    console.log("Y");
+    console.log("A");
+    console.log("N");
+    console.log("S");
+    console.log("H");
+}
+
+//myName();//Function call to execute the function
+
+function addTwoNumbers (num1,num2){
+    console.log(num1 + num2);
+}
+
+addTwoNumbers(10,20);//Function call to execute the function//30
+const result1 = addTwoNumbers(10,20);//Function call to execute the function
+console.log(result1);//undefined, because the function does not return anything
+
+function addNumbers (num1,num2){
+    let res = num1 + num2;//Scope of 'res' is within the braces of function
+    return res;//terminates the function and return the value at functiion call position
+}
+
+const result = addTwoNumbers(10,20);//its scope is outside the function
+console.log(result);//30
+
+function UserNameMessage(username){
+    // if (username === undefined){
+    //     console.log("Please enter a username");
+    //     return;
+    // }
+    //or
+    if (!username){
+        console.log("Please enter a username");
+        return;
+    }
+    return `${username}, Welcome to the world of JavaScript`;
+}
+
+console.log(UserNameMessage("Suryansh"));//Suryansh, Welcome to the world of JavaScript
+console.log(UserNameMessage());//Please enter a username
+console.log(UserNameMessage("")); //undefined, because the function does not return anything
+
+//Default parameters
+
+function UserNameMessageWithDefault(username = "Surya"){
+    //This if block is not needed because we have set a default value for username
+    if (!username){
+        console.log("Please enter a username");
+        return;
+    }
+    return `${username}, Welcome to the world of JavaScript`;
+}
+
+console.log(UserNameMessageWithDefault());//Surya, Welcome to the world of JavaScript
---
+++ 03_basics/02_functionWithObjects.js
@@ -0,0 +1,40 @@
+//Rest operator allows us to pass an indefinite number of arguments to a function(...args) and it will be stored in an array.
+function calculateCartPrice(...num1){
+    return num1;
+}
+
+console.log(calculateCartPrice(10, 20, 30, 40)); // [10, 20, 30, 40]
+
+function calculatePrice(val1,val2,...num1){//
+    return num1;
+}
+
+console.log(calculatePrice(10, 20, 30, 40)); // [30, 40]
+
+const user = {
+    name: "Surya",
+    price: 199
+}
+
+function handleObject(anyUser){
+    console.log(`Username is ${anyUser.name} and price is ${anyUser.price}`);
+}
+//handleObject(user); // Username is Surya and price is 199
+handleObject({
+    name: "Surya",
+    price: 199
+});
+
+function handleObjects(anyUser){
+    console.log(`Username is ${anyUser.name} and price is ${anyUser.prices}`);
+}
+
+handleObjects(user)// Username is Surya and price is undefined
+
+
+//Passing arrays to functions
+let MyArray = [10, 20, 30, 40];
+function getVal(arr){
+    return MyArray[2];//3rd value of array as 30 
+}
+console.log(getVal(MyArray)); // 30
