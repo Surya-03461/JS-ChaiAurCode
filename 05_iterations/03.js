b64073ef11664d1c3ef436cf715fe900e8bf8981
ggsipu <suryansh030806@gmail.com>
2025-07-21 15:02:34 +0000

    for of!!

---
+++ 05_iterations/03.js
@@ -0,0 +1,32 @@
+//for of loop
+
+//["","",""]
+//[{},{},{}]
+
+let arr = [1,2,3,4,5]
+for (const num of arr) {
+    console.log(num)
+}
+
+/* 
+1
+2
+3
+4
+5
+*/
+
+const name = "Suryansh"
+for (const char of name) {
+    console.log(`char is ${char}`)
+}
+/* 
+char is S
+char is u
+char is r
+char is y
+char is a
+char is n
+char is s
+char is h
+*/
